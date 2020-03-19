e791d1e1de18a0f80120b19a068a770b4d65193e
vermin <vermin@techie.com>
2020-03-19 05:45:52 +0100

    waznized and v1.0.0

---
+++ README.md
@@ -0,0 +1,29 @@
+WAZN Android Miner
+======================
+
+[![License](https://img.shields.io/badge/license-GPL--3.0-blue)](https://opensource.org/licenses/GPL-3.0)
+
+Optimized android miner running on a blockchain created for WAZN Project.
+
+## Usage
+
+Miner application will work on devices with ARM64 architecture.
+
+Install and run the app, enter your wallet address, press start button to initiate mining or press stop to suspend mining.  
+Connections are available through [wazn-proxy](https://github.com/project-wazn/wazn-proxy) and mining is made on a server running [wazn-nodejs-pool](https://github.com/project-wazn/wazn-nodejs-pool).
+
+## Notes
+
+The xmrig binary is copied to the app's internal directory along with its dependent libraries as it can only be executed from that location.
+
+The binary is started using the ProcessBuilder class, and the output is captured into the application scrolling pane once each second.
+
+Currently only arm64 binaries are included, and the app will refuse to work on other architectures like x86 or 32 bit devices.
+
+## License
+```
+Licensed under the GPL-3.0
+Copyright (c) 2019 WAZN Project
+Copyright (c) 2019 uPlexa
+Copyright (c) 2017-2019 xmrig
+```
---
+++ app/src/main/java/wazn1/wazn/androidminer/SettingsFragment.java
@@ -0,0 +1,90 @@
+package wazn1.wazn.androidminer;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class SettingsFragment extends Fragment {
+    public static TextView data;
+    public static EditText edUser;
+    public static EditText edThreshold;
+    public static EditText edWorkerID;
+    public static String wallet;
+    public static String workerid;
+    public static float minpay;
+    Button click;
+
+    @Nullable
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false  );
+        Context appContext = MainActivity.getContextOfApplication();
+        data = view.findViewById(R.id.fetchdata);
+        click = view.findViewById(R.id.saveSettings);
+        edUser = view.findViewById(R.id.username);
+        edThreshold = view.findViewById(R.id.threshold);
+        edWorkerID = view.findViewById(R.id.workerID);
+
+
+
+
+
+        if(PreferenceHelper.getName() != null) { edUser.setText(PreferenceHelper.getName()); }
+        if(PreferenceHelper.getThreshold() != null) { if(Float.parseFloat(PreferenceHelper.getThreshold()) > 75){ edThreshold.setText(PreferenceHelper.getThreshold()); } else{ edThreshold.setText("10.00"); }}
+        if(PreferenceHelper.getWorkerID() != null && PreferenceHelper.getWorkerID().length() >0) { edWorkerID.setText(PreferenceHelper.getWorkerID()); } else{ edWorkerID.setText("MyAndroidDevice"); }
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Save data.
+                wallet = edUser.getText().toString().trim();
+                workerid = edWorkerID.getText().toString().trim();
+                PreferenceHelper.setName(wallet);
+                minpay = Float.parseFloat(edThreshold.getText().toString());
+                if(minpay < 10) {
+                    Toast.makeText(appContext, "Minimum Pay Too Low!", Toast.LENGTH_SHORT).show();
+                }else {
+                    PreferenceHelper.setThreshold(minpay);
+                    if(!workerid.contains(" ")) {
+                        //success, lets post it!
+                        PreferenceHelper.setWorkerID(workerid);
+                        MiningService.workerId = workerid;
+                        Toast.makeText(appContext, "Saved!", Toast.LENGTH_SHORT).show();
+
+                        postData process = new postData();
+                        process.execute();
+
+
+                    }else{
+                        Toast.makeText(appContext, "Worker name may not contain spaces!", Toast.LENGTH_SHORT).show();
+
+
+
+                    }
+                }
+            }
+        });
+        return view;
+
+    }
+}
